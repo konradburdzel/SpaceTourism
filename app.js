3bb7b3424583d217ac868d8dfb967fa82a17b80f
konradburdzel <kburdzel0@gmail.com>
2022-12-21 01:38:23 +0100

    Dodanie aktualnej wersji projektu roboczego

---
+++ app.js
@@ -0,0 +1,23 @@
+
+//mobile
+const hamburgerMenu = document.querySelector('.hamburger');
+
+hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('active');
+})
+
+//tablet
+const menuLinks = [...document.querySelectorAll('.menu-link')];
+const getActive = (e) => {
+    const linkClasses = e.target.classList;
+    menuLinks.forEach(menuLink => {
+        menuLink.classList.remove('actived');
+    })
+    linkClasses.add('actived');
+}
+
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', getActive);
+})
+
+
